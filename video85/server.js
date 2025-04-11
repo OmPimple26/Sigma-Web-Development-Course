360be0d8af85b6224435eb2a1cf85c0cce564484
OmPimple26 <ompimple04@gmail.com>
2025-04-11 15:17:52 +0530

    Video85 Initial Commit

---
+++ video85/server.js
@@ -0,0 +1,11 @@
+// console.log("Hello World Om")
+
+var slugify = require('slugify')
+
+let a = slugify('some string') // some-string
+console.log(a)
+
+// if you prefer something other than '-' as separator
+// const b = slugify('some string', '_')  // some_string
+const b = slugify('some st&&*(^%$$^^&ring', '_')  // some_string
+console.log(b)
