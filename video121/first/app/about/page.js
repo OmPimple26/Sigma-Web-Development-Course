04fba12fc6bb455373719199691c7e8aa508bfb2
Om Pimple <ompimple04@gmail.com>
2025-08-19 00:40:25 +0530

    Video121 Initial Commit

---
+++ video121/first/app/about/page.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const page = () => {
+  return (
+    // <div>I am about</div>
+    <div>
+      <h1 className="text-xl text-green-500">Problems solved by NextJS</h1>
+      <ul>
+        <li>Full Stack Solution</li>
+        <li>File Based Routing</li>
+        <li>Additional features like router from next/navigation</li>
+        <li>Optimized rendering</li>
+      </ul>
+    </div>
+  );
+};
+
+export default page;
